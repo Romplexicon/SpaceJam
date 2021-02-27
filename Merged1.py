52966a44487d3073cae440faf5bd7346a2de828b
Anish-1507 <77978943+Anish-1507@users.noreply.github.com>
2021-02-27 17:20:32 +0530

    Add files via upload

---
+++ Merged1.py
@@ -0,0 +1,117 @@
+from datetime import date
+from openpyxl import Workbook, load_workbook
+
+# wb=Workbook()
+# No longer required after creation of excel sheet
+wb = load_workbook('/Users/Anish/Downloads/DataRepo.xlsx')
+ws = wb.active
+# Opens a sheet to start working on
+ws.title = "Input Data"
+wb.save('/Users/Anish/Downloads/DataRepo.xlsx')
+flag = 0
+global count
+count = 1
+global count_positive
+count_positive = 0
+global High_Risk_Sections
+High_Risk_Sections = ['']
+
+
+class severity():
+    today = date.today()
+    d1 = today.strftime("%Y/%m/%d")
+    d = d1.split("/")
+    status = ""
+    str = ""
+    test = input("Please enter if you tested negative for covid 19 after testing positive : ")
+    for i in d:
+        str += i
+    try:
+        date_all = int(str)
+    except:
+        print("Found an error in time..Cannot continue")
+    print(f"Today's date is {d1}")
+    D2 = input("Please enter the date you last tested negative for Covid-19. Enter in the form of YY/MM/DD : ")
+    d2 = D2.split("/")
+    str2 = ""
+    for i in d2:
+        str2 += i
+        date_test = int(str2)
+    f_date = date(int(d[0]), int(d[1]), int(d[2]))
+    l_date = date(int(d2[0]), int(d2[1]), int(d2[2]))
+    delta = f_date - l_date
+    x = delta.days
+    if x >= 90:
+        status = "High Risk"
+    else:
+        status = "Low Risk"
+
+a = severity()
+
+
+# Format of Data Storage: Name, Severity, Location
+class user:
+    global N, D, S, L, C
+    N = str()
+    S = str()
+    L = str()
+    C = str()
+
+    def input(self):
+        global N, D, S, L
+        N = input("Enter your name : ")
+        C = input("Enter your section : ")
+        D = a.D2
+        S = a.status
+        L = input("Vector location @abhay")
+    if S == "High Risk":
+        High_Risk_Sections.append(C)
+    print(High_Risk_Sections)
+
+
+global U
+U = user()
+U.input()  # This inputs
+
+
+# Function to write
+def write():
+    # global count
+    # global U
+    SN = ws.cell(row=count, column=1)
+    SN.value = N
+    SD = ws.cell(row=count, column=2)
+    SD.value = D
+    SS = ws.cell(row=count, column=3)
+    SS.value = S
+    SL = ws.cell(row=count, column=4)
+    SL.value = L
+    SC = ws.cell(row=count, column=5)
+    SC.value = C
+    wb.save('/Users/Anish/Downloads/DataRepo.xlsx')
+
+
+write()
+# Works till here
+'''
+wb.save('DataRepo.xlsx')
+#saves the data
+'''
+
+
+# Function to read
+def read():
+    SN = ws.cell(row=count, column=1)
+    N = SN.value
+    SD = ws.cell(row=count, column=2)
+    D = SD.value
+    SS = ws.cell(row=count, column=3)
+    S = SS.value
+    SL = ws.cell(row=count, column=4)
+    L = SL.value
+
+
+# function usage
+
+write()
+read()
