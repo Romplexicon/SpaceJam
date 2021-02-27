b3240e99971a63233cee725f23322aa9e7629558
AsbestosLampshade <77981488+AsbestosLampshade@users.noreply.github.com>
2021-02-27 11:31:44 +0530

    0.1
    Uhhhhh, I suck at classes

--- Excel Manipulation spacejam.py
+++ Excel Manipulation spacejam.py
@@ -1,16 +1,34 @@
 from openpyxl import Workbook
-wb= load_workbook('DataRepo.xlsx')
+from openpyxl import load_workbook
+wb = load_workbook('DataRepo.xlsx')
 ws= wb.active
 #Opens a sheet to start working on
 ws.title="Input Data"
-wb.save('DataRepo.xlsx')
-#saves the data
 flag=0
+global count
+count=1
 #Format of Data Storage: Name, Severity, Location
 class user:
     N=input("Enter your name")
+    D=input("Date tested")
     S=input("Calculated severity @Anish")
     L=input("Vector location @abhay")
-    
+
+global U
+U=user()
 while(flag!=1):
-    
+    #global count
+    #global U
+    SN=ws.cell(row=count,column=1)
+    SN.value=U.N
+    SD=ws.cell(row=count,column=2)
+    SD.value=U.D
+    SS=ws.cell(row=count,column=3)
+    SS.value=U.S
+    SL=ws.cell(row=count,column=4)
+    SL.value=U.L
+    count=count+1
+    wb.save('DataRepo.xlsx')
+
+wb.save('DataRepo.xlsx')
+#saves the data
